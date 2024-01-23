d479d83707be845400b5948ebc90617bca6609c1
anhkhoanghg <khoanguyen.vt2015@gmail.com>
2024-01-24 03:14:59 +0800

    add focal loss

--- src/models/components/focal_loss.py
+++ src/models/components/focal_loss.py
@@ -16,14 +16,16 @@ class FocalLoss(nn.Module):
         self.size_average = size_average
 
     def forward(self, input, target):
-        if input.dim() > 2:
-            # N,C,H,W => N,C,H*W
-            input = input.view(input.size(0), input.size(1), -1)
-            input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1, 1)
+        target = torch.tensor(target.clone(), dtype=torch.int64)
 
-        logpt = F.log_softmax(input)
+        # if input.dim() > 2:
+        #     # N,C,H,W => N,C,H*W
+        #     input = input.view(input.size(0), input.size(1), -1)
+        #     input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
+        #     input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
+        # target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
         logpt = logpt.gather(1, target)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
