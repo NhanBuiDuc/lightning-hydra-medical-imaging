74cf4ebf9481d26a696b3452e68697c3432c962b
anhkhoanghg <khoanguyen.vt2015@gmail.com>
2024-02-04 16:38:02 +0700

    update

--- src/data/isbi_datamodule.py
+++ src/data/isbi_datamodule.py
@@ -194,7 +194,8 @@ class IsbiDataModule(LightningDataModule):
 
                 # Assuming you have WeightedRandomSampler, you can use it like this:
                 self.weighted_sampler_train = WeightedRandomSampler(
-                    weights=[8, 2],
+                    weights=[(0.8*self.batch_size) // 100,
+                             (0.2*self.batch_size) // 100],
                     num_samples=len(train_label_data),
                     replacement=False
                 )
@@ -205,7 +206,8 @@ class IsbiDataModule(LightningDataModule):
 
                 # Assuming you have WeightedRandomSampler, you can use it like this:
                 self.weighted_sampler_val = WeightedRandomSampler(
-                    weights=[8, 2],
+                    weights=[(0.8*self.batch_size) // 100,
+                             (0.2*self.batch_size) // 100],
                     num_samples=len(val_label_data),
                     replacement=False
                 )
